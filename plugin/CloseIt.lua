5e4c1c33804e7a3e02baf7094f3d70387fd9ce04
Loosefish <Loosefish@users.noreply.github.com>
2016-07-24 19:54:00 +0200

    Rewrite in Lua

---
+++ plugin/CloseIt.lua
@@ -0,0 +1,159 @@
+-- Knowing if a closing char would be part of a string can be tricky.
+-- Not possible without a list of delimiters per language?
+-- What about delimiters with multiple chars?
+--
+-- One could initially insert a char in vim and check it's class (BARF!).
+-- Or call a second function after insertion which checks (also BARF!).
+--
+-- How it works now:
+-- s String
+-- " Delimiter
+-- x Not string
+-- _ End of line (nothing)
+-- | Cursor (column = position of second char)
+-- ^ Beginning of line
+--
+-- s|s  -> true +
+-- s|"  -> true +
+-- s|x  -> true (ex. bash var in "")
+-- s|_  -> true +
+--
+-- "|s  -> true +
+-- "|"  -> true +
+-- "|x  -> false +
+-- "|_  -> !is_string(line, col - 1) +
+--
+-- x|s  -> false
+-- x|"  -> false +
+-- x|x  -> false +
+-- x|_  -> false +
+--
+-- ^|s  -> true +
+-- ^|"  -> false + better: check end of prev. line
+-- ^|x  -> false +
+-- ^|_  -> false +
+
+local all = "([%(%)%[%]%{%}])"
+local open = {['['] = ']'; ['{'] = '}'; ['('] = ')'}
+local string_pattern = "[sS][tT][rR][iI][nN][gG]"
+local string_delim = "[\"']"
+
+-- Char types enum
+local STRING = 0
+local DELIM = 1
+local END = 2
+local OTHER = 3
+
+
+function setup(args)
+    -- Can we keep buffer local state in lua somehow?
+    all = "(["
+    open = {}
+    iter = string.gmatch(vim.eval("&matchpairs"), "(.):(.),?")
+    for o,c in iter do
+        all = all .. "%" .. o .. "%" .. c
+        open[o] = c
+    end
+    all = all .. "])"
+end
+
+
+function char_type(line, col)
+    local syn_name = vim.eval('synIDattr(synID(' .. line .. ',' .. col .. ', 0), "name")')
+
+    if syn_name == "" then
+        return END
+    end
+
+    if syn_name:find(string_pattern) then
+        if string.sub(vim.window().buffer[line], col, col):find(string_delim) then
+            return DELIM
+        end
+        return STRING
+    end
+
+    return OTHER
+end
+
+
+local function is_string(line, col)
+    local this = char_type(line, col)
+
+    if col > 1 then
+        local pre = char_type(line, col - 1)
+        if pre == OTHER or pre == END then
+            return false
+        elseif pre == STRING then
+            return true
+        else  -- pre == DELIM
+            if this == STRING or this == DELIM then
+                return true
+            elseif this == END then
+                return not is_string(line, col - 1)
+            else
+                return false
+            end
+        end
+    end
+
+    return this == STRING
+end
+
+
+local function find_in_line(line, col, in_string, stack, autoline)
+    local rev = string.reverse(vim.window().buffer[line])
+
+    if col then
+        col = #rev + 1 - col
+    else
+        col = 1
+    end
+
+    while true do
+        local i, _, char = string.find(rev, all, col)
+        if i then
+            col = i + 1
+            -- only process match if same as original
+            if in_string == is_string(line, #rev + 1 - i) then
+                if open[char] ~= nil then
+                    -- found opener -> check stack
+                    if #stack == 0 then
+                        -- stack empty -> done
+                        if autoline and i == 1 then
+                            return "\" .. open[char]
+                        else
+                            return open[char]
+                        end
+                    elseif stack[#stack] == open[char] then
+                        -- matches top -> pop
+                        stack[#stack] = nil
+                    else
+                        -- no match -> push
+                        stack[#stack + 1] = char
+                    end
+                else
+                    -- found closer -> push
+                    stack[#stack + 1] = char
+                end
+            end
+        elseif line > 1 then
+            -- check previous line
+            return find_in_line(line - 1, nil, in_string, stack, autoline)
+        else
+            -- no more lines
+            return nil
+        end
+    end
+end
+
+
+function find_closer(line, col, autoline)
+    setup()
+    local in_string = is_string(line, col)
+    if col > 1 then
+        return find_in_line(line, col - 1, in_string, {}, autoline)
+    elseif line > 1 then
+        return find_in_line(line - 1, nil, in_string, {}, autoline)
+    end
+    return nil
+end
--- plugin/CloseIt.vim
+++ plugin/CloseIt.vim
@@ -1,126 +1,18 @@
-" Configuration {{{ "
-if !exists("g:CloseItTrigger")
+execute 'luafile ' . expand('<sfile>:p:h') . '/CloseIt.lua'
+
+" Configuration
+if !exists('g:CloseItTrigger')
 	let g:CloseItTrigger = "<S-SPACE>"
 endif
-execute "inoremap " . g:CloseItTrigger . " <C-\\><C-o>:call <SID>CloseItAuto()<CR>"
-
-let s:defaultClosers = { '(': ')', '{': '}', '[': ']' }
-" }}} Configuration "
+execute "inoremap <expr> " . g:CloseItTrigger ." <SID>CloseItLua()"
 
 
-function! s:CloseItAuto() " {{{1 "
-	if exists("b:CloseItPairs")
-		let s:closers = b:CloseItPairs
-	elseif exists("g:CloseItPairs")
-		let s:closers = g:CloseItPairs
-	else
-		let s:closers = s:defaultClosers
-	endif
-	let s:openPattern = '\M' . join(keys(s:closers), '\|')
-
-	" Check if cursor is inside a string
-	if col('.') ==# col('$')
-		let inString = s:isStringEx(line('.'), col('.') - 1)
-	else
-		let inString = s:isStringEx(line('.'), col('.'))
-	endif
-	if inString
-		return s:CloseItInString()
-	else
-		return s:CloseIt()
-	endif
-endfunction " 1}}}
+function! s:CloseItLua()
+    let closer = luaeval('find_closer(' . line('.') . ',' . col('.') . ',true)')
+    if type(closer) == 0
+        return ''
+    endif
+    return closer
+endfunction
 
 
-function! s:CloseIt() " {{{1
-	let startview = winsaveview()
-	let startpos = getcurpos()
-
-	let until = max([0, startview['lnum'] - 1024])
-	while search(s:openPattern, 'Wb', until) > 0
-		if !s:isClosed(startpos[1], startpos[2], 's:isString()') && !s:isString()
-			call s:insertCloser(startview)
-			return 1
-		endif
-	endwhile
-	call winrestview(startview)
-	return 0
-endfunction " 1}}}
-
-
-function! s:CloseItInString() " {{{1
-	let startview = winsaveview()
-	let startpos = getcurpos()
-
-	while search(s:openPattern, 'Wb') > 0
-		if !s:isString()
-			break
-		endif
-		if !s:isClosed(startpos[1], startpos[2], '!s:isString()')
-			call s:insertCloser(startview)
-			return 1
-		endif
-	endwhile
-	" no pending opener inside strings - try regular
-	call winrestview(startview)
-
-	if s:CloseIt()
-		" since the opener is not part of a string we remove the inserted char
-		" if it is inside a string
-		let inString = s:isStringEx(line('.'), col('.'))
-
-		if inString
-			if col('.') ==# col('$') - 1
-				normal! x
-			else
-				normal! X
-			endif
-			call winrestview(startview)
-			return 0
-		else
-			return 1
-		endif
-	endif
-endfunction " 1}}}
-
-
-function! s:isClosed(beforeline, beforecol, skip) " {{{1
-	let startview = winsaveview()
-	let opener = getline('.')[col('.') - 1]
-	let [mline, mcol] = searchpairpos('\M' . opener, '', '\M' . s:closers[opener], 'W', a:skip, a:beforeline)
-	if [mline, mcol] ==# [0, 0]
-		let closed = 0
-	elseif mline ==# a:beforeline
-		let closed = mcol <= a:beforecol
-	else
-		let closed = mline <= a:beforeline
-	endif
-	call winrestview(startview)
-	return closed
-endfunction " 1}}}
-
-
-function! s:insertCloser(startview) " {{{1
-	let closeChar = s:closers[getline(".")[col(".") - 1]]
-	if col('.') + 1 ==# col('$') && a:startview['lnum'] !=# line('.')
-		let closeChar = "\<CR>" . closeChar
-	endif
-	call winrestview(a:startview)
-	if a:startview['col'] == 0 && col('$') == 1  " Empty line
-		execute 'normal! a' . closeChar . "\<ESC>$"
-	elseif a:startview['col'] == col('$') - 1  " End of line
-		execute 'normal! a' . closeChar . "\<ESC>$"
-	else
-		execute 'normal! i' . closeChar . "\<RIGHT>"
-	endif
-endfunction " 1}}}
-
-
-function! s:isString() " {{{1
-	return synIDattr(synID(line("."), col("."), 0), "name") =~? "string"
-endfunction " 1}}}
-
-
-function! s:isStringEx(l, c) " {{{1
-	return synIDattr(synID(a:l, a:c, 0), "name") =~? "string"
-endfunction " 1}}}
